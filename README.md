5df98a72bb79f5cb582ebfcdd97dc9222337cb4b
Wilian Ansanello <wiliansanello@outlook.com>
2024-02-19 05:41:53 -0300

    Commit inicial do projeto

---
+++ README.md
@@ -0,0 +1,8 @@
+# Projeto Gestão Vagas
+
+## Tecnologias utilizadas
+
+- [Spring Boot](spring.io)
+- Docker
+- PostgreSQL
+- Swagger
---
+++ src/main/java/br/com/wiliansanello/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,21 @@
+package br.com.wiliansanello.gestao_vagas.modules.candidate.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.wiliansanello.gestao_vagas.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+
+    @PostMapping("/")
+    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        System.out.println("Candidato");
+        System.out.println(candidateEntity.getEmail());
+    }
+    
+}
