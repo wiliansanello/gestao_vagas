8717b60b881a00dd466faee45c97ca88a47e5eb4
Wilian Ansanello <wiliansanello@outlook.com>
2024-04-23 06:30:51 -0300

    Teste banco de dados em memória

--- src/test/java/br/com/wiliansanello/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
+++ src/test/java/br/com/wiliansanello/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -20,17 +21,23 @@ import org.springframework.web.context.WebApplicationContext;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.wiliansanello.gestao_vagas.modules.company.dto.CreateJobDTO;
+import br.com.wiliansanello.gestao_vagas.modules.company.entities.CompanyEntity;
+import br.com.wiliansanello.gestao_vagas.modules.company.repositories.CompanyRepository;
 import br.com.wiliansanello.gestao_vagas.utils.TestUtils;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class CreateJobControllerTest {
 
     private MockMvc mvc;
 
     @Autowired
     private WebApplicationContext context;
+
+    @Autowired
+    private CompanyRepository companyRepository;
 
     @Before
     public void setup() {
@@ -41,6 +48,15 @@ public class CreateJobControllerTest {
 
     @Test
     public void shouldBeAbleToCreateANewJob() throws Exception {
+
+        var company = CompanyEntity.builder()
+            .description("COMPANY_DESCRIPTION")
+            .email("email@company.com")
+            .password("1234567890")
+            .username("COMPANY_USERNAME")
+            .name("COMPANY_NAME").build();
+        
+        company = companyRepository.saveAndFlush(company);
 
         var createdJobDTO = CreateJobDTO.builder()
         .benefits("BENEFITS_TEST")
@@ -53,7 +69,7 @@ public class CreateJobControllerTest {
         .content(
             TestUtils.objectToJSON(createdJobDTO))
             .header("Authorization", 
-                TestUtils.generateToken(UUID.fromString("173124ba-62e2-4719-8481-c1d5c774e8cc"), 
+                TestUtils.generateToken(company.getId(), 
                     "JAVAGAS_@123#")))
         .andExpect(MockMvcResultMatchers.status().isOk());
 
