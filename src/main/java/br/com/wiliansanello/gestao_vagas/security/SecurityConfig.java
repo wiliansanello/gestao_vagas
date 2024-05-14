5e30689b4159395978c7af953b8652bbfef8b721
Wilian Ansanello <wiliansanello@outlook.com>
2024-05-14 08:14:26 -0300

    Configuracao Prometheus com erro para inicializar

--- src/main/java/br/com/wiliansanello/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/wiliansanello/gestao_vagas/security/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
         "/swagger-ui/**",
         "/v3/api-docs/**",
         "/swagger-resources/**",
-        "/actuator/*"
+        "/actuator/**"
     };
 
     @Bean
